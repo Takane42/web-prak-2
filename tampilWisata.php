ce64a4f2a2c1f33d9e8ab17f5152cd0073c0aa7e
Takane42 <dragster1616@gmail.com>
2025-05-03 21:49:37 +0700

    First Commit

---
+++ tampilWisata.php
@@ -0,0 +1,32 @@
+<?php
+
+function curl($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
+$send = curl("http://localhost/rekayasaweb/praktikum2/getWisata.php");
+
+echo "<h1>Data Wisata</h1>
+    <table border='1' cellpadding='10' cellspacing='0'>
+    <tr>
+    <th>KOTA</th>
+    <th>LANDMARK</th>
+    <th>TARIF</th>
+    </tr>";
+
+$data = json_decode($send, true);
+
+foreach ($data as $row) {
+    echo "<tr>";
+    echo "<td>" . $row['kota'] . "</td>";
+    echo "<td>" . $row['landmark'] . "</td>";
+    echo "<td>" . $row['tarif'] . "</td>";
+    echo "</tr>";
+}
+?>
